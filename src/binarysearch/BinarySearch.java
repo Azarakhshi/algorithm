8c582817663a2efa955794174a74e9104a6b7609
azarakhshi-m <azarakhshi.mohammadamin@gmail.com>
2024-08-07 16:34:30 +0330

    create the binary search algorithm with java

---
+++ src/binarysearch/BinarySearch.java
@@ -0,0 +1,13 @@
+package binarysearch;
+
+public class BinarySearch {
+
+    int binarySearch(int number, int[] array){
+        int low = 0;
+        int high = array.length - 1;
+
+        return 0;
+
+    }
+
+}
