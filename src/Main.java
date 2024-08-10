e4d9dc619bb7821ddd96076d3d941dc36ab39016
azarakhshi-m <azarakhshi.mohammadamin@gmail.com>
2024-08-10 12:36:27 +0330

    test the binary search

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import binarysearch.BinarySearch;
+
+
 public class Main {
     public static void main(String[] args) {
+
+        //binary search
+        BinarySearch binarySearch = new BinarySearch();
+        int[] array = {1, 5, 9, 10, 55, 59, 97, 102, 555, 1258};
+        int number = 555;
+        System.out.println("the number found with binary search { " + binarySearch.binarySearch(number, array) + " }");
 
 
     }
--- src/binarysearch/BinarySearch.java
+++ src/binarysearch/BinarySearch.java
@@ -2,9 +2,24 @@ package binarysearch;
 
 public class BinarySearch {
 
-    int binarySearch(int number, int[] array){
+    public int binarySearch(int number, int[] array) {
+
         int low = 0;
         int high = array.length - 1;
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            if (number == array[mid]) {
+                return array[mid];
+            } else if (number < array[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+
+        }
 
         return 0;
 
